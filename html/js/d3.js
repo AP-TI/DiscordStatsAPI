af52b0fb4597990d10bce477e933c2e3e71658ed
mixxamm <maximjanssensj5@gmail.com>
2020-02-05 13:36:28 +0100

    Gebruik database aangemaakt door DiscordStatsCore

--- html/js/d3.js
+++ html/js/d3.js
@@ -1,42 +1,41 @@
-d3.json("http://localhost:3000").then((data) => {
-    data.forEach(d => d.aantal.numberInt = +d.aantal.numberInt);
+d3.json("http://localhost:3000")
+  .then(data => {
+    //data.forEach(d => d.aantal.numberInt = +d.aantal.numberInt);
 
-    var svg = d3.select("#data-here").append("svg")
-        .attr("width", 300)
-        .attr("height", 300);
+    var svg = d3
+      .select("#data-here")
+      .append("svg")
+      .attr("width", data.aantal)
+      .attr("height", 300);
 
-    var circles = svg.selectAll("circle")
-        .data(data);
+    var circles = svg.selectAll("circle").data(data);
 
-    circles.enter()
-        .append("rect")
-        .attr("x", (d, idx) => {
-            return idx;
-        })
-        .attr("y", function (d, idx) {
-            return 200;
-        })
-        .attr("height", function (d) {
-            var x = d.aantal.numberInt;
+    circles
+      .enter()
+      .append("rect")
+      .attr("x", (d, idx) => {
+        return idx;
+      })
+      .attr("y", function(d, idx) {
+        return 200;
+      })
+      .attr("height", function(d) {
+        var x = d.aantal;
 
-            if (x <= 0) return 0;
+        if (x <= 0) return 0;
 
-            return x;
-        })
-        .attr("width", "2px")
-        .attr("fill", (d) => {
-            if (d.aantal.numberInt > 50)
-                return 'rgb(204,0,0)';
-            else if (d.aantal.numberInt > 40)
-                return 'rgb(204,102,0)';
-            else if (d.aantal.numberInt > 30)
-                return 'rgb(204,204,0)';
-            else if (d.aantal.numberInt > 20)
-                return 'rgb(102,204,0)';
-            else if (d.aantal.numberInt > 10)
-                return 'rgb(0,0,204)';
-            else
-                return 'rgb(0,0,0';
-        });
-}).catch(error => { console.log(error) })
-
+        return x;
+      })
+      .attr("width", "2px")
+      .attr("fill", d => {
+        if (d.aantal > 50) return "rgb(204,0,0)";
+        else if (d.aantal > 40) return "rgb(204,102,0)";
+        else if (d.aantal > 30) return "rgb(204,204,0)";
+        else if (d.aantal > 20) return "rgb(102,204,0)";
+        else if (d.aantal > 10) return "rgb(0,0,204)";
+        else return "rgb(0,0,0";
+      });
+  })
+  .catch(error => {
+    console.log(error);
+  });
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require("mongodb").MongoClient;
 var db;
 
-MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+MongoClient.connect("mongodb://localhost:27017", (err, client) => {
   if (err) return console.log(err);
-  db = client.db('apti')
-})
-
+  db = client.db("apti");
+});
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  db.collection('discord').find().toArray((err, data) => {
-    if (err) throw err;
-    res.json(data);
-  })
+router.get("/", function(req, res, next) {
+  db.collection("aantalOnline")
+    .find()
+    .toArray((err, data) => {
+      if (err) throw err;
+      res.json(data);
+    });
 });
 
 module.exports = router;
